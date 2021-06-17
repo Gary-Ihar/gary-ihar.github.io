abb20a4f70f7979e91c7b7f00089d19a26d1148e
Gary-Ihar <yakuzawot6@gmail.com>
2021-06-17 15:21:33 +0300

    add websocket logic

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 const getSelector = selectorName => document.querySelector(selectorName);
-const prodURL = 'https://app-list-server.herokuapp.com'; // prod
-const localHost = 'http://localhost:80'; // dev
+const prodURL = 'ws://app-list-server.herokuapp.com'; // prod
+const localHost = 'ws://localhost:80'; // dev
 const ul = getSelector('.list-section__list');
 const productName = getSelector('input[name="product"]');
 const productQnty = getSelector('input[name="qnty"]');
 const btnSendForm = getSelector('#sendForm');
 const btnClearList = getSelector('#clearList');
+const statusIndicator = getSelector('.status_indicator');
+const actions = { // as backend
+    ADD_ITEM: "ADD_ITEM",
+    REMOVE_ITEM: "REMOVE_ITEM",
+    EDIT_ITEM: "EDIT_ITEM",
+    CLEAR_LIST: "CLEAR_LIST",
+};
 
-let url = prodURL;
+let url = localHost;
 const modeInfo = getSelector('.active_mode');
 const btnChangeMode = getSelector('#change_mode');
+
+let socketIsOpen = false;
+
+const socket = new WebSocket(url);
+
+socket.onopen = function() {
+    socketIsOpen = true;
+    setOnlineStatus(socketIsOpen);
+    socket.send(JSON.stringify('init'));
+};
+
+socket.onclose = function() {
+    socketIsOpen = false;
+    setOnlineStatus(socketIsOpen);
+};
+  
+socket.onerror = function() {
+    socketIsOpen = false;
+    setOnlineStatus(socketIsOpen);
+};
+
+socket.onmessage = function(event) {
+    const data = JSON.parse(event.data);
+    liCreator(data);
+};
 
 btnChangeMode.addEventListener('click', () => {
     if (modeInfo.textContent === 'off') {
@@ -19,47 +51,30 @@ btnChangeMode.addEventListener('click', () => {
         modeInfo.textContent = 'off';
         url = prodURL;
     }
-    updateListItem();
 });
 
-btnSendForm.addEventListener('click', async (event) => {
+btnSendForm.addEventListener('click', (event) => {
     event.preventDefault();
-    const body = {
-        productName: productName.value,
-        productQnty: productQnty.value,
-    };
-    await fetch(`${url}/list`,{
-        method: 'POST', 
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(body),
-    })
-    .catch(() => {
-        console.log('что-то пошло не так, элемент не добавился'); // TODO: обработать ошибку.
-    })
-    .finally(updateListItem)
-    productName.value = '';
-    productQnty.value = '';
+    if(socketIsOpen) {
+        const data = {
+            productName: productName.value,
+            productQnty: productQnty.value,
+        };
+        socket.send(configureData(actions.ADD_ITEM, data));
+        productName.value = '';
+        productQnty.value = '';
+    }
 });
 
 btnClearList.addEventListener('click', () => {
-    fetch(`${url}/clear`)
-    .catch(()=>{
-        console.log('что-то пошло не так, список не очистился') // TODO: обработать ошибку.
-    })
-    .finally(updateListItem)
-})
+    if(socketIsOpen) {
+        socket.send(configureData(actions.CLEAR_LIST, {}));
+    };
+});
 
-function updateListItem() {
-    fetch(`${url}/get`)    
-    .then((res)=>res.json())
-    .then(res=>{
-        liCreator(res);
-    })
-    .catch(()=>{
-        console.log('что-то пошло не так, список не обновился глобально') // TODO: обработать ошибку.
-    })
+function setOnlineStatus(change) {
+    if (change) statusIndicator.classList.add('status_on');
+    else statusIndicator.classList.remove('status_on');
 };
 
 function liCreator(list) { // list - array
@@ -79,4 +94,9 @@ function liCreator(list) { // list - array
     });
 };
 
-updateListItem();
+function configureData(action, data){
+    return JSON.stringify({
+        action,
+        data
+    })
+};
